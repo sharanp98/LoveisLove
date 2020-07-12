56b8fa31d2589059266548c92b38a29c7ba99cb7
Sharan <sharanp98@gmail.com>
2020-07-12 20:36:31 +0530

    created training file

--- scraping/merge.py
+++ scraping/merge.py
@@ -1,7 +1,7 @@
 # merge three dataframes
 import pandas as pd
 
-df1 = pd.read_csv('train_negative.csv', index_col=0)
+df1 = pd.read_csv('train_negative_clean.csv', index_col=0)
 df2 = pd.read_csv('train_positive.csv', index_col=0)
 df3 = pd.read_csv('train_neutral.csv', index_col=0)
 
