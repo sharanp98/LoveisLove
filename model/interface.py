b822e3d708000f33d7caf5b2c81eebb6a98d5867
JACOBIN-SCTCS <jacobja3@gmail.com>
2020-07-15 12:56:35 +0530

    Added predictions file results.csv using interface.py

--- model/interface.py
+++ model/interface.py
@@ -1,7 +1,11 @@
 
 from model import SentimentModel
 from transformers import AutoTokenizer,AutoModel
+import pandas as pd
+import csv
 import torch
+
+s = [ "negative","random","positive"]
 
 
 bert_model = AutoModel.from_pretrained('google/bert_uncased_L-4_H-256_A-4')
@@ -12,17 +16,41 @@ model.lstm.load_state_dict(torch.load('rnn.pth'))
 model.layers.load_state_dict(torch.load('checkpoint.pth'))
 model.eval()
 
-text = [ "happy" ,
-        "of course gay men dress well. They didnt spend all that time in the closet doing nothing",
-      "Be calm because it is okay to be gay " , "it is very happy to know you are sad", "i am going to be sad",
-      "married with pride", "100 years of atlantic stories","we will not be erased","few say being LGBT is negative",
-      "Extremism in the defense of liberty is no vice. And moderation in the pursuit of justice is no virtue",
-      "All young people, regardless of sexual orientation or identity, deserve a safe and supportive environment in which to achieve their full potential.",
-      "Labels are for filing. Labels are for clothing. Labels are not for people.",
-      ]
+f = open('result.csv','w',newline='') 
+out_file = csv.writer(f)
 
-for t in text:
-    out=model([t],bert_tokenizer)
-    print(out)
+data = pd.read_csv('results_ocr.csv')
 
-#print(model)
+
+
+for idx,row in data.iterrows():
+    if type(row['Text']) == float:
+        out_file.writerow([row['Filename'],"random"])
+    else:
+        scores = model([row['Text']],bert_tokenizer)
+        sentiment = torch.argmax(torch.exp(scores),dim=1)
+        out_file.writerow([row['Filename'],s[sentiment]])
+
+f.close()
+
+
+
+#print(data.head())
+
+
+#text = [ "happy" ,
+#        "of course gay men dress well. They didnt spend all that time in the closet doing nothing",
+#      "Be calm because it is okay to be gay " , "it is very happy to know you are sad", "i am going to be sad",
+#      "married with pride", "100 years of atlantic stories","we will not be erased","few say being LGBT is negative",
+#      "Extremism in the defense of liberty is no vice. And moderation in the pursuit of justice is no virtue",
+#      "All young people, regardless of sexual orientation or identity, deserve a safe and supportive environment in which to achieve their full potential.",
+#      "Labels are for filing. Labels are for clothing. Labels are not for people.",
+#      ]
+
+#for t in text:
+#    out=model([t],bert_tokenizer)
+#    print(out)
+
+
+
+
